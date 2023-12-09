10fa3c0b01a761a74c24a68b4de8ed2f74f18495
Isaac Isvaldo Bunga <isaac.bunga@mamboo.co.ao>
2023-12-09 15:26:14 +0100

    feat:configurated the base of web services

---
+++ WEB/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+ export default function Home() {
+  return (
+  <div className="h-screen bg-zinc-900 text-zinc-50 text-center">
+    <h1 className="text-4xl font-bold">Sua cápsula do Tempo !</h1>
+  </div>
+
+  )
+}
