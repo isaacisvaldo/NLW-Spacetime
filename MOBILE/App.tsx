52e54bb855f5fafae3469fdece6cdd7be3911364
Isaac Isvaldo <91834847+IsaacIsvaldo@users.noreply.github.com>
2023-12-10 16:21:29 +0100

    feat:config base App

---
+++ MOBILE/App.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 font-bold text-2xl ">Minha Primeira Aplicação!</Text>
+      <StatusBar style="light" translucent/>
+    </View>
+  );
+}
+
+
+ 
