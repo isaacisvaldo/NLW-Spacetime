b540a05385f0f02a863ba0e404cbb5ef5cc990c7
Isaac Isvaldo Bunga <isaac.bunga@mamboo.co.ao>
2023-12-14 18:04:04 +0100

    feat:update data

--- WEB/src/app/page.tsx
+++ WEB/src/app/page.tsx
@@ -1,9 +1,22 @@
 
  export default function Home() {
-  return (
-  <div className="h-screen bg-zinc-900 text-zinc-50 text-center">
-    <h1 className="text-4xl font-bold">Sua cápsula do Tempo !</h1>
+  return  <main className="grid grid-cols-2 min-h-screen">
+  {/*Left */}
+  <div className=" relative flex flex-col items-start justify-between px-28 py-16  overflow-hidden">
+     {/*Blur */}
+    <div className="absolute right-0 top-1/2  h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  rounded-full bg-purple-700 opacity-50 blur-full "/>
+ {/*Strips*/}
+ <div className="absolute right-0 top-0 bottom-0 w-2 bg-str"/>
   </div>
-
-  )
+  {/*Right */}
+  <div className="flex flex-col p-16">
+  <div className="flex flex-1 items-center justify-center">
+     <p className="text-center leading-relaxed w-[360px]">
+      Você ainda não registrou nenhuma lembrança, comece a {' '}
+       <a href="" className="underline hover:text-gray-50">criar agora</a> 
+       !
+       </p>
+  </div>
+  </div>
+  </main>
 }
